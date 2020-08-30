62f2c19b7819cf42e08341fd0c40b863588c5951
totolog <totolog1126@gmail.com>
2020-08-30 18:42:15 +0900

    Finish

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'totologstore.herokuapp.com'
+  host = 'https://totologstore.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
