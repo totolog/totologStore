cd7151736a3f4a9556e26e13754b557a8409fe6c
totolog <totolog1126@gmail.com>
2020-08-30 15:53:34 +0900

    Add account activation

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby '2.6.6'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.3'
 gem "bcrypt", "3.1.12"
+gem 'faker',          '1.7.3'
 # Use sqlite3 as the database for Active Record
 # gem 'sqlite3'
 # Use Puma as the app server
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name:  "Example User",
+                email: "example@railstutorial.org",
+                tel: "00000000000",
+                password:              "foobar",
+                password_confirmation: "foobar",
+                admin: true,
+                activated: true,
+                activated_at: Time.zone.now)
+
+99.times do |n|
+    name  = Faker::Name.name
+    email = "example-#{n+1}@railstutorial.org"
+    tel = "0000000000#{n+1}"
+    password = "password"
+    User.create!(name:  name,
+                    email: email,
+                    tel: tel,
+                    password:              password,
+                    password_confirmation: password,
+                    activated: true,
+                    activated_at: Time.zone.now)
+end
